377dc5780e75c2b7b9d350f259b03ed7396970af
Ssoppii <138455014+Ssoppii@users.noreply.github.com>
2024-02-11 01:33:00 +0900

    Time: 1425 ms (13.04%), Space: 17.9 MB (64.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,13 +1,9 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int ans = nums.size();
-        for (int i=0;i<nums.size();i++) {
-            if (find(nums.begin(), nums.end(), i) == nums.end()) {
-                ans = i;
-                break;
-            }
-        }
-        return ans;
-    }
-};
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        ans = len(nums)
+        for i in range(len(nums)):
+            if i not in nums:
+                ans = i
+                break
+        
+        return ans
